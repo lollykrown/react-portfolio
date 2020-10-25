c0b8a2a7d411735884a75bd496906e00ba4e9118
lollykrown <joe_kayu@yahoo.com>
2020-10-25 21:01:21 +0100

    .

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -141,21 +141,6 @@ const HomeWrapper = styled.div`
     cursor:grab;   
 }
   
-//   @media (prefers-reduced-motion: no-preference) {
-//     .arrow {
-//       animation: App-logo-spin infinite 20s linear;
-//     }
-//   }
-  
-// keyframes App-logo-spin {
-//     from {
-//       transform: rotate(0deg);
-//     }
-//     to {
-//       transform: rotate(360deg);
-//     }
-//   }
-
   .row {
     display:grid;
     grid-template-columns: 50% 50%;
